30c92f4aebaeef49f34f970e345b9242d3b33d20
agun36 <aguntoroti10@gmail.com>
2023-11-06 16:54:27 +0000

    main

--- backend/main.py
+++ backend/main.py
@@ -51,6 +51,9 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 models.Base.metadata.create_all(bind=engine) 
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 @app.post("/posts_blog/", response_model=UserModel)
 async def create_post(posts_blog: UserBase, db: db_dependency):
