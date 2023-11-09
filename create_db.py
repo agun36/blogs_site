6100733d2841b82964a848095390836ee10f39e5
agun36 <aguntoroti10@gmail.com>
2023-11-09 13:50:06 +0000

    i just reconnect to mysql

---
+++ create_db.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password123",
+    # auth_plugin="mysql_native_password"
+)
+
+# it makes all execute queries
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE our_users")
+mycursor.execute("SHOW DATABASES")
+
+for db in mycursor:
+    print(db)
