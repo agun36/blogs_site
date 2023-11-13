683bd58325bd4b656c84aa1025846f88f8204c07
agun36 <aguntoroti10@gmail.com>
2023-11-13 22:26:03 +0000

    main

--- app.py
+++ app.py
@@ -165,4 +165,4 @@ def name():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True,host="0.0.0.0",port=int(os.envion.get("PORT",8080)))
+    app.run(debug=True,host="0.0.0.0",port=int(os.environ.get("PORT",8080)))
